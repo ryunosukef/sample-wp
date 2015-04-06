eac2e4f8ef4a15bf31c4f82e4725f892de75011f
R.Fujimoto <fujimoto.ryunosuke@gmail.com>
2015-04-07 00:49:23 +0900

    create wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -3,7 +3,7 @@
  * WordPress の基本設定
  *
  * このファイルは、MySQL、テーブル接頭辞、秘密鍵、ABSPATH の設定を含みます。
- * より詳しい情報は {@link http://wpdocs.sourceforge.jp/wp-config.php_%E3%81%AE%E7%B7%A8%E9%9B%86 
+ * より詳しい情報は {@link http://wpdocs.sourceforge.jp/wp-config.php_%E3%81%AE%E7%B7%A8%E9%9B%86
  * wp-config.php の編集} を参照してください。MySQL の設定情報はホスティング先より入手できます。
  *
  * このファイルはインストール時に wp-config.php 作成ウィザードが利用します。
@@ -13,7 +13,7 @@
  * @package WordPress
  */
 
-// 注意: 
+// 注意:
 // Windows の "メモ帳" でこのファイルを編集しないでください !
 // 問題なく使えるテキストエディタ
 // (http://wpdocs.sourceforge.jp/Codex:%E8%AB%87%E8%A9%B1%E5%AE%A4 参照)
@@ -47,14 +47,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         'J.fI (jU~=@U=*ow&Yt|5Ny_u>Jj}x^tc4EZeDH _Eip;{?7*X+pJiIYko[4b>-6');
+define('SECURE_AUTH_KEY',  'Ik)zdRx23B:^u5:78-#1y IXQCqwryb?Nr]X/hqy^Xwa,f&tn?1JR]Y7NU> 5v|s');
+define('LOGGED_IN_KEY',    '-sVM8+!-t:w]1|lC+&t{67Kf5BGEEPkcAVdj_e[cM-b;9lR@YgEqeMNQn,vP2}7+');
+define('NONCE_KEY',        '[zQ`:|1oGe,~!T/^tIjrlsOIwA{X,v3<1I`7g-6cK_oSx/%2Z@d/YpDGYt&Itt<N');
+define('AUTH_SALT',        'uv,YO9&b<d:GinZzDY^U>zvsN+Asd;c]@x66 D/JA!!nOFw_H1E0UvsZo#},u~KZ');
+define('SECURE_AUTH_SALT', '4!)S?vF1h=VR=Jm?5n[|ST[Z2[j+I*0IUCL?+H/f.Sr>&4~1ZJBNoeAvD!FXO:r!');
+define('LOGGED_IN_SALT',   'Ae)U3Z763V+z`Fx0^Wy|FMh+P7Gs|zReAC<k=KYWxn)27b+m(1JY6K~,n6|RGi1<');
+define('NONCE_SALT',       'sL [e!SPg?pe?r=DM4JPMfc7+0KBSxX{JS+:q6@q~2yI[PxQD#i+eKJ1J/gE^a[j');
 
 /**#@-*/
 
@@ -82,3 +82,6 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
+define('WP_HOME', 'http://'.$_SERVER['HTTP_HOST']);
+define('WP_SITEURL', 'http://'.$_SERVER['HTTP_HOST']);
